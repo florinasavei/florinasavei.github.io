d45f45fa4afba079a51fe9bbd76943e0f0282c2e
florin.asavei <florin.asavei@principal33.com>
2024-08-28 23:08:40 +0300

    re-test deployment

--- code/fe/portfolio/apps/landing-page/src/app/nx-welcome.tsx
+++ code/fe/portfolio/apps/landing-page/src/app/nx-welcome.tsx
@@ -432,7 +432,7 @@ export function NxWelcome({ title }: { title: string }) {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome {title} 👋
+              Welcome {title} V1 👋
             </h1>
           </div>
 
